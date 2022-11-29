3ac3594f141299decb75f905ebaf2986023795bd
eu-d3 <ze_3fanuel@proton.me>
2022-11-29 18:14:24 +0300

    PyCodeStyle Validated

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -20,4 +20,4 @@ elif last_digit == 0:
 elif last_digit < 6:
     string = "and is less than 6 and not 0"
 
-print(f"Last digit of {number:d} is {last_digit:d}"," ",string)
+print(f"Last digit of {number:d} is {last_digit:d}", " ", string)
