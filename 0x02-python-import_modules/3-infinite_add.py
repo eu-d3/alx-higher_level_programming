7c94d6737999e2336e805531452ac56b97b63886
eu-d3 <ze_3fanuel@proton.me>
2022-12-01 14:34:31 +0300

    Adds successive args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+"""Print the addition of all arguments."""
+
+
+if __name__ == "__main__":
+    
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
