6ad56a8985e936b263bb0c2f9e58b87f99c41fb3
eu-d3 <ze_3fanuel@proton.me>
2022-12-02 23:28:27 +0300

    Initialize directory

---
+++ 0x03-python-data_structures/make_py.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+import_modules () {
+	echo "Would you like to import any module? "
+	read modules
+	while [ "$modules" != "nvm" ]; do
+		echo -e "import $modules" >> $file_name.py
+		read modules
+	done
+
+	if [ "$modules" == "nvm" ]; then
+		echo -e "\n" >> $file_name.py
+	fi
+}
+
+add_comments () {
+	echo "Would you like to add any comments? "
+	read comment
+	if [ "$comment" != "" ]; then
+		echo -e "\"\"\"$comment\"\"\"" >> $file_name.py
+	else 
+		echo -e "\n" >> $file_name.py
+	fi
+}
+check_style () {
+	echo -e "You might have not adhered to PEP8 regulations,\n
+	If no messages follow this message, all is good!
+        "
+        pycodestyle $file_name.py
+	echo -e "Did you spot any errors, If so, "
+        echo "Rectify: "
+        read req
+        if [ "$req" == "y" ]; then
+        	vim $file_name.py
+                pycodestyle $file_name.py
+                echo "Done? "
+                read assert2
+                if [ "$assert2" == "y" ]; then
+                	git add $file_name.py
+                        git commit -m "Rectified coding style, PEP8"
+		else
+			check_style
+		fi
+	fi
+}
+
+init_file () {
+	echo "What is the name of the file you would like to create:
+Do not add the .py extension!"
+	read file_name
+	echo -e "#!/usr/bin/python3\n" > $file_name.py
+	import_modules
+	add_comments
+	edit_file
+	check_style
+}
+edit_file () {
+	vim $file_name.py
+	chmod a+x $file_name.py
+	echo "Would you like to run the file $file_name.py you just created:
+Type y to run any other key to skip: "
+	read query
+	if [ "$query" == "y" ]; then
+		./$file_name.py
+		echo "Did it run correctly? Would you like to stage and commit? "
+		read response
+		if [ "$response" == "y" ]; then
+			git add $file_name.py
+			echo "Type your commit message for $file_name.py: "
+			read commit_message
+			echo "You are about to commit with the message	$commit_message"
+			read assert
+			if [ "$assert" == "y" ]; then
+				git commit -m "$commit_message"
+			fi
+		fi
+	fi
+}
+init_file
