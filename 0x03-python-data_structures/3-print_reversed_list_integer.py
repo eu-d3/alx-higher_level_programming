7d21054e9a4de1cca3e7ca854b66e9b8ad877384
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 00:25:26 +0300

    Prints a list of integers in reversed order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""Prints a list of integers in reversed order"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    if in_it(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
