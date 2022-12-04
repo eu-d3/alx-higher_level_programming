3ae23689ce6f6965c933d76aed7e020af40fc0e4
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 00:27:32 +0300

    Used prebuilt function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if in_it(my_list, list):
+    if isinstance(my_list, list):
         my_list.reverse()
         for i in my_list:
             print("{:d}".format(i))
