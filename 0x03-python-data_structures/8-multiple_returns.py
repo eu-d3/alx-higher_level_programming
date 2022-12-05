7adfdab17cc3a0f2656fd4620ba20e4a8ecd0349
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 19:59:32 +0300

    Length plus first element

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+""""Returns the length of a string and its first character."""
+
+
+
+def multiple_returns(sentence):
+     if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
