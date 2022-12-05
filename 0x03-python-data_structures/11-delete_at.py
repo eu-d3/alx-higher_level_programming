5f457b34a0fd5f3dcebb79786ec0646dc774346b
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 20:18:16 +0300

    Rectified coding style. PEP8

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,6 @@
 """Deletes an item at a specific position in a list."""
 
 
-
 def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,7 @@
 """"Returns the length of a string and its first character."""
 
 
-
 def multiple_returns(sentence):
-     if sentence == "":
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
