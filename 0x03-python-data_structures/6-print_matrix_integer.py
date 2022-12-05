ef2e5656650ffccda04cd49805e7644f9ad6961c
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 19:28:48 +0300

    A matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Prints a matrix of integers to STDOUT"""
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
