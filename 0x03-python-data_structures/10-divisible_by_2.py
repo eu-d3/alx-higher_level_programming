aef95f5a5e13f0a147a96a405e9ce4a657e8d060
eu-d3 <ze_3fanuel@proton.me>
2022-12-05 10:06:46 -0800

    Handles special case in which len(list) is 0

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,8 @@
 
 
 def divisible_by_2(my_list=[]):
+    if list_len == 0:
+                return None
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             multiples.append(True)
