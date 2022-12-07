a76ee99b310c6034ca5ca9732b18d1e47949d4b7
eu-d3 <ze_3fanuel@proton.me>
2022-12-07 19:20:56 +0300

    Adds/Replaces items in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""Replace or add key/value pairs to a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
